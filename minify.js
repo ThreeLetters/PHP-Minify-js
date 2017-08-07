3f6a46eefa87c82b7e896384df03c7c20ae71535
Andrew S <andrews54757@gmail.com>
2017-08-07 13:27:26 -0400

    Update minify.js

--- minify.js
+++ minify.js
@@ -1,1 +1,68 @@
+function minify(str) {
+    str = str.replace(/\n/g, "").split("");
+    var len = str.length;
+    var out = [];
+    var i = 0;
 
+    function skip(match) {
+
+        var backslash = false;
+        out.push(match);
+
+        for (++i; i < len; i++) {
+            char = str[i];
+            out.push(char);
+            if (char === "\\") backslash = true;
+            else if (char === match && !backslash) {
+                break;
+            } else if (backslash) {
+                backslash = false;
+            }
+        }
+    }
+
+    function includes(char) {
+
+        var dt = [";", "{", "}", ",", "(", ")", "[", "]", "=", ">", "<", "."];
+        return dt.indexOf(char) != -1;
+    }
+
+    function includes2(char) {
+        var dt = ["=", "{", "(", "}", ")", "]", ">", "<", "!", ".", "$"];
+
+        return dt.indexOf(char) != -1;
+    }
+    for (; i < len; i++) {
+        var char = str[i];
+
+        if (char == "\"") {
+            skip("\"");
+        } else if (char == "'") {
+            skip("'");
+        } else if (char == "`") {
+            skip("`");
+        } else if (char == " ") {
+
+            var d = true;
+
+            for (; i < len; i++) {
+
+                if (includes2(str[i + 1])) {
+                    d = false;
+                }
+                if (str[i + 1] != " ") break;
+            }
+            if (d) out.push(" ");
+
+        } else if (includes(char)) {
+            out.push(char);
+            for (; i < len; i++) {
+                if (str[i + 1] != " ") break;
+            }
+        } else {
+            out.push(char)
+        }
+
+    }
+    return out.join("");
+}
